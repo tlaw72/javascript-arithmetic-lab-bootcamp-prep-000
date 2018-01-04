631e45d1b557a671720aeb871eec94724d6c3026
Troy Lawrence <redirect@gmail.com>
2018-01-04 19:13:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ function divide (a, b) {
 function inc (n) {
   return n++;
 }
-inc ();
-
 function dec (n) {
   return n--;
 }
-dec();
+function makeInt (n) {
+  return parseInt(n, 10);
+}
+function preserveDecimal (n) {
+  return parseFloat(n);
+}
